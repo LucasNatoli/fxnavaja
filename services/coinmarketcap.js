a1461d8555426ee9fd1d403f5a5b112a629d7f1e
Lucas Natoli <lucas.natoli@gmail.com>
2018-08-31 12:48:27 -0300

    delete last read before insert

--- services/coinmarketcap.js
+++ services/coinmarketcap.js
@@ -23,41 +23,43 @@ function updateTicker(db){
   getTicker().then(
     data=> {
       if (data.length>0) {
- /*        db.coinmarketcapTick.destroy({
+        db.coinmarketcapTick.destroy({
           where: {},
           truncate: true
-        }) */
+        })
+        .then(()=>{
 
-        for(var i=0; i<data.length; i++){
-          var t = data[i]
-          db.coinmarketcapTick
-          .findOrCreate({
-            where: {
-              market_id: t.id, 
-              last_updated: t.last_updated
-            }, 
-            defaults: {
-              market_id: t.id,
-              symbol: t.symbol,
-              last_updated: t.last_updated,
-              name: t.name,
-              rank: t.rank,
-              price_usd: t.price_usd,
-              price_btc: t.price_btc,
-              vol_24h_usd: t['24h_volume_usd'],
-              market_cap_usd: t.market_cap_usd,
-              available_supply: t.available_supply,
-              total_supply: t.total_supply ,
-              max_supply: t.max_supply,
-              percent_change_1h: t.percent_change_1h,
-              percent_change_24h: t.percent_change_24h,
-              percent_change_7d: t.percent_change_7d
-            }
-          })
-          .spread((tick, created)=> {
-            if (created) console.log(tick.get({plain:true}))
-          })
-        }
+          for(var i=0; i<data.length; i++){
+            var t = data[i]
+            db.coinmarketcapTick
+            .findOrCreate({
+              where: {
+                market_id: t.id, 
+                last_updated: t.last_updated
+              }, 
+              defaults: {
+                market_id: t.id,
+                symbol: t.symbol,
+                last_updated: t.last_updated,
+                name: t.name,
+                rank: t.rank,
+                price_usd: t.price_usd,
+                price_btc: t.price_btc,
+                vol_24h_usd: t['24h_volume_usd'],
+                market_cap_usd: t.market_cap_usd,
+                available_supply: t.available_supply,
+                total_supply: t.total_supply ,
+                max_supply: t.max_supply,
+                percent_change_1h: t.percent_change_1h,
+                percent_change_24h: t.percent_change_24h,
+                percent_change_7d: t.percent_change_7d
+              }
+            })
+            .spread((tick, created)=> {
+              if (created) console.log(tick.get({plain:true}))
+            })
+          }
+        })
       }
     },
     err => {
