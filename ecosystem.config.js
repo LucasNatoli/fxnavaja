13bc4d8cc8954758b4080aabccff98198d7eaab4
Lucas Natoli <lucas.natoli@gmail.com>
2018-08-31 15:31:55 -0300

    d

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,11 +36,12 @@ module.exports = {
    */
   deploy : {
     production : {
+      key  : '/home/lucas/.ssh/id_rsa.pub',
       user : 'razor',
       host : 'fxnavaja.com',
       ref  : 'origin/master',
       repo : 'https://github.com/LucasNatoli/fxnavaja.git',
-      path : 'fxnavaja',
+      path : '/var/www/fxnavaja',
       'post-deploy' : 'npm install && pm2 start ecosystem.config.js --env production'
     }
   }
