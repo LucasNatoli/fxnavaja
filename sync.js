baca7ef9f347462780e564e3a3dd1a0e9a3f2be1
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-07 13:58:49 -0300

    added promise and process.exit() to sync script

--- sync.js
+++ sync.js
@@ -1,3 +1,13 @@
 const db = require('./db')
 
 db.sequelize.sync()
+.then(
+  (r)=>{
+    console.log("db sync ok")
+    process.exit()
+  },
+  (err)=>{
+    console.log('db sync error', err)
+    process.exit()
+  }
+)
