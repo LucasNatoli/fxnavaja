5041d5cd9bdb31ac171bcf502fb7b16f5705c09b
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-07 17:47:57 -0300

    sync script for dropping old alarm tables and scan bookmarks

--- sync.js
+++ sync.js
@@ -1,13 +1,23 @@
 const db = require('./db')
-
-db.sequelize.sync()
-.then(
-  (r)=>{
-    console.log("db sync ok")
-    process.exit()
-  },
-  (err)=>{
-    console.log('db sync error', err)
-    process.exit()
+db.scanBookmark.drop().then(
+  ()=> {
+    db.alarmSuscription.drop().then(
+      ()=> {
+        db.alarm.drop().then(
+          ()=> {
+            db.sequelize.sync().then(
+              ()=>{
+                console.log("db sync ok")
+                process.exit()
+              },
+              (err)=>{
+                console.log('db sync error', err)
+                process.exit()
+              }
+            )        
+          }
+        )     
+      }
+    )  
   }
-)
+)  
