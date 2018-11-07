ad42a81bd37b37ef5b03cbc92aedb83e92899694
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-07 13:36:13 -0300

    remove promise catch from sync

--- sync.js
+++ sync.js
@@ -1,12 +1,3 @@
 const db = require('./db')
 
-db.sequelize
-.sync()
-.then(
-  (r)=>{
-    console.log("db sync ok")
-  },
-  (err)=>{
-    console.log('db sync error', err)
-  }
-)
+db.sequelize.sync()
