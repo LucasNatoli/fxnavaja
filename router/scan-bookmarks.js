a8371ea0ba47f71a83da584c126911ab4979128c
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-07 16:46:07 -0300

    enhaced /scan-bookmarks response

--- router/scan-bookmarks.js
+++ router/scan-bookmarks.js
@@ -5,17 +5,38 @@ module.exports = (app, db) => {
    
     var sess = req.session
     if (typeof sess.userid != 'undefined'){
-      db.account.findOne({where: {userid:sess.userid}})
+      db.account.findOne({
+        where: {userid:sess.userid},
+        include: [{model: db.scanBookmark, include: [db.scanProfile]}]
+      })
       .then(
         account=>{
-          account.getScanBookmarks
-          .then(
-            bookmarks=>{res.json(bookmarks)},
-            err=>{
-              res.status(500)
-              console.log('error: ', err)
-            }
+          
+          res.json(
+            account.scanBookmarks.map(
+              bm=>{ 
+                return {
+                  active: bm.active, 
+                  scanProfile: {
+                    id: bm.scanProfile.id, 
+                    exchange: bm.scanProfile.exchange,
+                    coin: bm.scanProfile.coin,
+                    asset: bm.scanProfile.asset,
+                    interval: bm.scanProfile.interval
+                  }
+                }
+              }
+            )
           )
+          
+          // account.getScanBookmarks()
+          // .then(
+          //   bookmarks=>{res.json(bookmarks)},
+          //   err=>{
+          //     res.status(500)
+          //     console.log('error: ', err)
+          //   }
+          // )
         }
       )
     } else {
@@ -23,22 +44,22 @@ module.exports = (app, db) => {
     }
   })
 
-  // app.post('/scan-bookmarks/', (req, res) => {
+  app.post('/scan-bookmarks/', (req, res) => {
  
-  //   var exchange = req.body.exchange
-  //   var coin = req.body.coin
-  //   var asset = req.body.asset
-  //   var interval = req.body.interval
+    var exchange = req.body.exchange
+    var coin = req.body.coin
+    var asset = req.body.asset
+    var interval = req.body.interval
 
-  //   db.scanBookmark.create({
-  //     exchange: exchange,
-  //     coin: coin,
-  //     asset: asset,
-  //     interval: interval,
-  //     limit: limit
-  //   })
-  //   .then(bookmark => {
-  //     res.json(bookmark)
-  //   })
-  // })
+    db.scanBookmark.create({
+      exchange: exchange,
+      coin: coin,
+      asset: asset,
+      interval: interval,
+      limit: limit
+    })
+    .then(bookmark => {
+      res.json(bookmark)
+    })
+  })
 }
