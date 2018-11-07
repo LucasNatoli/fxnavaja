63bbaef072ab632148c50f607e148a6257e98dad
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-07 17:54:04 -0300

    fixed some sync bug

--- sync.js
+++ sync.js
@@ -1,23 +1,13 @@
 const db = require('./db')
-db.scanBookmark.drop().then(
-  ()=> {
-    db.alarmSuscription.drop().then(
-      ()=> {
-        db.alarm.drop().then(
-          ()=> {
-            db.sequelize.sync().then(
-              ()=>{
-                console.log("db sync ok")
-                process.exit()
-              },
-              (err)=>{
-                console.log('db sync error', err)
-                process.exit()
-              }
-            )        
-          }
-        )     
-      }
-    )  
+
+db.sequelize.sync()
+.then(
+  ()=>{
+    console.log("db sync ok")
+    process.exit()
+  },
+  (err)=>{
+    console.log('db sync error', err)
+    process.exit()
   }
-)  
+)        
