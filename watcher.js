d51f1d0d752bf404d0f2f45ed3b32bd1a13105b0
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-11 11:36:51 -0300

    disabling coinmarketcap ticker

--- watcher.js
+++ watcher.js
@@ -5,10 +5,10 @@ const db = require('./db')
 const coinmarketcapTiker = require('./services/coinmarketcap')
 const candlesScanner = require('./candles/scanner')
 
-new CronJob('0 */5 * * * *', function() {
-  var ticker = new coinmarketcapTiker(db)
-  ticker.updateTicker()
-}, null, true, 'America/Argentina/Buenos_Aires')
+// new CronJob('0 */5 * * * *', function() {
+//   var ticker = new coinmarketcapTiker(db)
+//   ticker.updateTicker()
+// }, null, true, 'America/Argentina/Buenos_Aires')
 
 new CronJob('*/60 * * * * *', function() {
 	db.scanProfile.findAll()
