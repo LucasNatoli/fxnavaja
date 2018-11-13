4aa6a27bb65c804e16bc10640e48c19d966ffab3
Lucas Natoli <lucas.natoli@gmail.com>
2018-11-13 15:52:44 -0300

    code enhacement. detected bug when candle array length < indicator input

--- utils/calc.js
+++ utils/calc.js
@@ -6,17 +6,12 @@ function getSources(candles, source) {
 }
 
 function Calc() {
-  this.isIndicator = expression => {
-    return !(typeof this[expression.split('_')[0]] === "undefined")
-  },
-  this.isCandleValue = expression => {
-    return ['O', 'H', 'L', 'C', 'v'].includes(expression)
-  },
 
-  this.BB = (candles, args) => {return boll(getSources(candles, args[0]), args[1], args[2])[args[3]]}
-  this.EMA = (candles, args) => {
-    return ma.ema(getSources(candles, args[0]), args[1])
-  }
+    //TODO: check candles length agains indicator input length
+  this.isIndicator = expression => {return !(typeof this[expression.split('_')[0]] === "undefined")},
+  this.isCandleValue = expression => {return ['O', 'H', 'L', 'C', 'v'].includes(expression)},
+  this.BB = (candles, args) => {return boll(getSources(candles, args[0]), args[1], args[2])[args[3]]},
+  this.EMA = (candles, args) => {return ma.ema(getSources(candles, args[0]), args[1])},
   this.MA = (candles, args) => {return ma.ma(getSources(candles, args[0]), args[1])}
 }
 
